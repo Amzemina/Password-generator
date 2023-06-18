b7a75d0271cae6e3f2eff9b6f2df3e72d49fecdb
Ashley Zemina <amzemina@gmail.com>
2023-06-17 20:25:17 -0400

    added js code

--- script.js
+++ script.js
@@ -1,5 +1,55 @@
 // Assignment code here
 
+function generatePassword() {
+
+var criteria = [];
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz" ;
+var number = "0123456789";
+var symbol= "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+var randPassword = ""; 
+
+
+var passLength = prompt("Choose password length between 8 and 128 characters .")
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
+    alert("Invalid entry - Please enter a number between 8 and 128");
+    return
+}
+
+var addUpper = confirm("Would you like UpperCase letters?");
+if (addUpper === true) {
+  criteria.push(upper);
+}
+
+var addLower = confirm("Would you like to include LowerCase letters?");
+if (addLower === true) {
+  criteria.push(lower);
+}
+
+var addNumber = confirm("would you like to include Numbers?");
+if (addNumber === true) {
+  criteria.push(number);
+}
+
+var addSymbol = confirm("Would you like to include Symbols?");
+if (addSymbol === true) {
+  criteria.push(symbol);
+}
+
+if( addUpper == false && addLower == false && addNumber == false && addSymbol == false) {
+  alert("invalid entry - Please choose at least one criteria.")
+  generatePassword();
+}
+
+for (var i = 0; i < passLength; i++) {
+    var randomCriteria = criteria[Math.floor(Math.random() * criteria.length)];
+    var randomCharacter = randomCriteria[Math.floor(Math.random() * randomCriteria.length)];
+    randPassword += randomCharacter;
+  }
+
+  return randPassword;
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
